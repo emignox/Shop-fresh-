b7bfc1e18ac4b07504117a9e470410e633be7d5f
emanuele <127688017+emignox@users.noreply.github.com>
2024-02-15 09:50:27 +0100

    Delete back directory

--- back/middleware/cors.js
+++
@@ -1,9 +0,0 @@
-const cors = require("cors");
-
-const corsMiddleware = cors({
-  origin: "http://localhost:5173",
-  methods: ["GET", "POST", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-});
-
-module.exports = corsMiddleware;
